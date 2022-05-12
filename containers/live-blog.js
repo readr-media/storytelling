3a5aca125e7b928276a649aef4f993b825bdb942
caesar <caesar.lee@mirrormedia.mg>
2022-05-12 16:38:19 +0800

    feat: implement live blog control

--- containers/live-blog.js
+++ containers/live-blog.js
@@ -1,10 +1,12 @@
 import Intro from '../components/Intro'
+import LiveBlogControl from '../components/LiveBlogControl'
 import LiveBlogWrapper from '../components/LiveBlogWrapper'
 
 export default function LiveBlogContainr() {
   return (
     <LiveBlogWrapper>
       <Intro />
+      <LiveBlogControl />
     </LiveBlogWrapper>
   )
 }
