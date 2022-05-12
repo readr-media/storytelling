6dbdfd9e4194b4f77734fd9f8097935aa1057f08
caesar <caesar.lee@mirrormedia.mg>
2022-05-12 22:10:04 +0800

    refactor: remove redundant code

--- containers/live-blog.js
+++ containers/live-blog.js
@@ -12,37 +12,3 @@ export default function LiveBlogContainr() {
     </LiveBlogWrapper>
   )
 }
-
-/*
-<LiveBlogWrapper>
-<LiveBlogItemsWrapper>
-  <LiveBlogControl />
-  <LiveBlogItems>
-    <PinedLiveBlog />
-    <LiveBlogItemWrapper>
-      <LiveBlogItem>
-        <LiveBlogControl />
-        <LiveBlogMeta>
-          <div>Tags</div>
-          <div>
-            <div>
-              <img />
-              <span>記者名字</span>
-            </div>
-            <div>
-              <span>日期</span>
-            </div>
-          </div>
-        </LiveBlogMeta>
-        <LiveBlogArticle>
-          <div>Title</div>
-          <div>Article...</div>
-          with collapse state
-        </LiveBlogArticle>
-        <LiveBlog></LiveBlog>
-      </LiveBlogItem>
-    </LiveBlogItemWrapper>
-  </LiveBlogItems>
-</LiveBlogItemsWrapper>
-</LiveBlogWrapper>
-*/
