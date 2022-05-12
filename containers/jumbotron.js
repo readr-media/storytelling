e6891e1826377c67e28e108ee8f2108606eefebb
caesar <caesar.lee@mirrormedia.mg>
2022-05-12 16:31:11 +0800

    refactor: separate heroImage from jumbotron

--- containers/jumbotron.js
+++ containers/jumbotron.js
@@ -1,15 +1,9 @@
-import Image from 'next/image'
+import HeroImage from '../components/HeroImage'
 
 export function JumbotronContainer() {
   return (
     <>
-      <Image
-        src="/images/hero-image.png"
-        width="1440"
-        height="484"
-        layout="responsive"
-        alt="Image of hero"
-      />
+      <HeroImage />
     </>
   )
 }
