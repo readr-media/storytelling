6982ba0e219c5d513cea57e26c29666961e76ac6
caesar <caesar.lee@mirrormedia.mg>
2022-05-16 17:11:22 +0800

    refactor: move custom font to the page liveblog

--- pages/liveblog/index.js
+++ pages/liveblog/index.js
@@ -1,6 +1,7 @@
 import { JumbotronContainer } from '../../containers/jumbotron'
 import LiveBlogContainr from '../../containers/live-blog'
 import { createGlobalStyle } from 'styled-components'
+import Head from 'next/head'
 
 const GlobalStyles = createGlobalStyle`
   body {
@@ -11,6 +12,15 @@ const GlobalStyles = createGlobalStyle`
 export default function LiveBlog() {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <div id="light-box-root" />
       <GlobalStyles />
       <JumbotronContainer />
