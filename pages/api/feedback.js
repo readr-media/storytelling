b714c6145848b2d9f12ebba2b27dfe893717defa
Po-Wei Chen <erase2004@gmail.com>
2022-05-20 12:25:58 +0800

    feat: add recaptcha assessment to /api/feedback

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,10 +1,15 @@
 import CORS from 'cors'
 import globalAPICall from '../../utils/api/globalAPICall'
 import publishMessage from '../../utils/api/publishMessage'
+import createAssessment from '../../utils/api/createAssessment'
+import { projectId } from '../../utils/api/config'
 
 const cors = CORS({
   methods: ['HEAD', 'GET', 'POST'],
 })
+
+const recaptchaSiteKey = '6LeHLAEgAAAAADb0pcN6CVZdgD7KFDtCFElRu-f7'
+const recaptchaScoreBoundary = 0.7
 
 // use helper function to run middleware
 function runMiddleware(req, res, fn) {
@@ -24,16 +29,30 @@ async function handler(req, res) {
   await runMiddleware(req, res, cors)
 
   async function POST() {
+    // recaptcha assessment
+    const score = await createAssessment({
+      projectID: projectId,
+      recaptchaSiteKey,
+      token: req.body.token,
+      recaptchaAction: req.body.recaptchaAction,
+    })
+
+    if (score === null || score < recaptchaScoreBoundary) {
+      return res.status(401).json({ message: 'recaptach assessment failed' })
+    }
+
+    // publish message to PubSub
     const result = await publishMessage(req.body)
 
     if (result === true) {
-      res.status(200).json({ status: 'success' })
+      res.status(200).json({})
     } else {
-      res.status(503).json({ status: 'failed', message: result })
+      res.status(503).json({ message: result })
     }
   }
 
   async function GET() {
+    // get feedbacks from keystone
     res.status(200).json({ message: 'empty' })
   }
 
