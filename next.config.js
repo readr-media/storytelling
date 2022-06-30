d9a18ce5b5331049d32b67b338633e83500e5964
caesar <caesar.lee@mirrormedia.mg>
2022-06-30 13:03:01 +0800

    chore(liveblog): renew gcs hostname

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['storage.googleapis.com'],
+    domains: ['editools-gcs-dev.readr.tw', 'editools-gcs.readr.tw'],
   },
 }
 
