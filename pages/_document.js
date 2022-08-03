7858f9c96463db94729563375559374b12d548d6
caesar <caesar.lee@mirrormedia.mg>
2022-08-03 18:34:32 +0800

    refactor(liveblog): set styled-components generate ssr stylesheet

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-export default function Document() {
+function CustomDocument() {
   return (
     <Html>
       <Head>
@@ -22,3 +23,29 @@ export default function Document() {
     </Html>
   )
 }
+
+CustomDocument.getInitialProps = async (ctx) => {
+  const sheet = new ServerStyleSheet()
+  const originalRenderPage = ctx.renderPage
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      })
+    const initialProps = await Document.getInitialProps(ctx)
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      ),
+    }
+  } finally {
+    sheet.seal()
+  }
+}
+
+export default CustomDocument
