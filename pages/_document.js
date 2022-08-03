b3070a6b923e53082fe8391a651fedb7debc645c
caesar <caesar.lee@mirrormedia.mg>
2022-08-03 18:50:19 +0800

    refactor(liveblog): remove unnecessary code

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,6 @@ function CustomDocument() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;700;900&display=swap"
           rel="stylesheet"
